0d0f7c65b34156e1a2aa4e12eb59f65e22373f12
leochocolat <l.mouraire@outlook.fr>
2019-02-03 21:36:16 +0100

    retours 3

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -4,3 +4,10 @@ $(".btn-menu").click(function(){
   $(".menu").toggleClass("menu-on");
 
 });
+
+
+//wall of ideas
+
+$(".btn-close-popup").click(function() {
+  TweenMax.to(".pop-up-container", .5, {autoAlpha: 0, ease: Power1.easeInOut});
+});
